547d251de487daf65256b2e3fc8de5bd76607268
Adam D <addstar34@gmail.com>
2015-09-28 03:03:39 +0000

    using TDD created a new feature to display the specific quote at quotes/id

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+  	quote = Quote.create(:author => 'Billy Bob', :saying => 'You only yolo once.')
+  	get :show, :id => quote.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'omg'
+  	assert_response :not_found
+  end
+
+
 end
